19f722087c45c975a637c53b12623e18b3ebad56
win <win.wen@sac-symtek.com>
2026-01-04 16:01:46 +0800

    增加path引用

--- src/App.vue
+++ src/App.vue
@@ -229,6 +229,9 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 import DxfUpload from './DxfUpload.vue'
 import { RenderManager } from './Renders/RenderManager'
+import { PathGeometry, PathPointList, PathTubeGeometry } from '/path/build/three.path.module.js';
+
+
 
 const showDxfUpload = ref(false)
 const isTopViewMode = ref(false)
